ee399cbf2e7a9fc53f6b4e2c588901f5b4a6f5f6
Joanx01 <joamorand@alu.edu.gva.es>
2025-01-26 19:50:53 +0100

    cambio en el sql

--- peliculas.sql
+++ peliculas.sql
@@ -1,41 +1,147 @@
-CREATE DATABASE IF NOT EXISTS `peliculas` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `peliculas`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-01-2025 a las 19:50:33
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
 
--- TABLA: USUARIOS
--- 0=invitado 1=general 2=admin
-CREATE TABLE `usuarios` (
-    `id_usuario` INT AUTO_INCREMENT PRIMARY KEY,
-    `nombre` VARCHAR(40) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `foto_perfil` VARCHAR(255) DEFAULT 'default.jpg',
-    `nivel_usuario` int(11) NOT NULL DEFAULT 0, 
-    `fecha_registro` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- TABLA: PELICULAS
-CREATE TABLE `peliculas` (
-    `id_pelicula` INT AUTO_INCREMENT PRIMARY KEY,
-    `titulo` VARCHAR(255) NOT NULL,
-    `descripción` TEXT NOT NULL,
-    `anio` YEAR NOT NULL,
-    `portada` VARCHAR(255) NOT NULL,
-    `categoria` VARCHAR(100) NOT NULL
-);
 
--- TABLA: COMENTARIOS
--- CASCADE SIGNIFICA QUE SI SE ELIMINA UN USUARIO/PELICULA, TAMBIÉN SE ELIMINAN LOS COMENTARIOS ASOCIADOS A EL.
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `peliculas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
 CREATE TABLE `comentarios` (
-    `id_comentario` INT AUTO_INCREMENT PRIMARY KEY,
-    `id_pelicula` INT NOT NULL,
-    `id_usuario` INT NOT NULL,
-    `contenido` TEXT NOT NULL,
-    `fecha` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`id_pelicula`) REFERENCES peliculas(id_pelicula) ON DELETE CASCADE, 
-    FOREIGN KEY (`id_usuario`) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
-);
+  `id_comentario` int(11) NOT NULL,
+  `id_pelicula` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- INSERCION DE PELICULAS EN TABLA PELICULAS
-INSERT INTO `peliculas` (`titulo`, `descripción`, `anio`, `portada`, `categoria`) VALUES 
-('La Era de Hielo', 'Una aventura en un mundo prehistórico', 2002, 'la_era_de_hielo.jpg', 'Animación'),
-('El Señor de los Anillos: La Comunidad del Anillo', 'El primer capítulo de una épica aventura', 2001, 'senor_de_los_anillos.jpg', 'Fantasía'),
-('Avengers: Endgame', 'La lucha final contra Thanos para salvar el universo', 2019, 'avengers_endgame.jpg', 'Acción');
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `id_pelicula` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripción` text NOT NULL,
+  `anio` year(4) NOT NULL,
+  `portada` varchar(255) NOT NULL,
+  `categoria` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id_pelicula`, `titulo`, `descripción`, `anio`, `portada`, `categoria`) VALUES
+(1, 'La Era de Hielo', 'Una aventura en un mundo prehistórico', '2002', 'la_era_de_hielo.jpg', 'Animación'),
+(2, 'El Señor de los Anillos: La Comunidad del Anillo', 'El primer capítulo de una épica aventura', '2001', 'senor_de_los_anillos.jpg', 'Fantasía'),
+(3, 'Avengers: Endgame', 'La lucha final contra Thanos para salvar el universo', '2019', 'avengers_endgame.jpg', 'Acción');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(550) NOT NULL,
+  `nombre_usuario` varchar(550) NOT NULL,
+  `contrasenya` varchar(255) NOT NULL,
+  `foto_perfil` varchar(255) DEFAULT 'default.jpg',
+  `nivel_usuario` int(11) NOT NULL DEFAULT 1,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `nombre_usuario`, `contrasenya`, `foto_perfil`, `nivel_usuario`, `fecha_registro`) VALUES
+(1, 'joan', 'morales', 'joan', '1234', 'default.jpg', 1, '2025-01-26 18:40:47');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_pelicula` (`id_pelicula`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id_pelicula`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id_pelicula`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
